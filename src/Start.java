66b84695cb2419cb5237c28b15277c63172b3c09
wellington <wellington@mint>
2016-02-29 23:20:59 -0300

    star main

--- src/Start.java
+++ src/Start.java
@@ -1,3 +1,7 @@
+
+import view.ViewPrincipal;
+import view.ViewSplash;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +13,10 @@
  * @author wellington
  */
 public class Start {
+    
+    public static void main(String args[]){
+        new ViewSplash().setVisible(true);
+        
+    }
     
 }
