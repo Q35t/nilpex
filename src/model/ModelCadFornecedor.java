f576c118e55d2320cf91289e0b3fb94554929768
wellington <wellington@mint>
2016-02-29 23:19:07 -0300

    model fornecedor

---
+++ src/model/ModelCadFornecedor.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author wellington
+ */
+public class ModelCadFornecedor {
+    
+    private int for_id;
+    private String for_nome;
+    private String for_cnpj_cpf;
+    private String for_email;
+    private String for_endereco;
+    private String for_num;
+    private String for_complemento;
+    private int for_idbairro;
+    private int for_idcidade;
+    private int for_idestado;
+    private String for_cep;
+    private String for_telefone;
+    private String for_telcomercial;
+
+    public int getFor_id() {
+        return for_id;
+    }
+
+    public void setFor_id(int for_id) {
+        this.for_id = for_id;
+    }
+
+    public String getFor_nome() {
+        return for_nome;
+    }
+
+    public void setFor_nome(String for_nome) {
+        this.for_nome = for_nome;
+    }
+
+    public String getFor_cnpj_cpf() {
+        return for_cnpj_cpf;
+    }
+
+    public void setFor_cnpj_cpf(String for_cnpj_cpf) {
+        this.for_cnpj_cpf = for_cnpj_cpf;
+    }
+
+    public String getFor_email() {
+        return for_email;
+    }
+
+    public void setFor_email(String for_email) {
+        this.for_email = for_email;
+    }
+
+    public String getFor_endereco() {
+        return for_endereco;
+    }
+
+    public void setFor_endereco(String for_endereco) {
+        this.for_endereco = for_endereco;
+    }
+
+    public String getFor_num() {
+        return for_num;
+    }
+
+    public void setFor_num(String for_num) {
+        this.for_num = for_num;
+    }
+
+    public String getFor_complemento() {
+        return for_complemento;
+    }
+
+    public void setFor_complemento(String for_complemento) {
+        this.for_complemento = for_complemento;
+    }
+
+    public int getFor_idbairro() {
+        return for_idbairro;
+    }
+
+    public void setFor_idbairro(int for_idbairro) {
+        this.for_idbairro = for_idbairro;
+    }
+
+    public int getFor_idcidade() {
+        return for_idcidade;
+    }
+
+    public void setFor_idcidade(int for_idcidade) {
+        this.for_idcidade = for_idcidade;
+    }
+
+    public int getFor_idestado() {
+        return for_idestado;
+    }
+
+    public void setFor_idestado(int for_idestado) {
+        this.for_idestado = for_idestado;
+    }
+
+    public String getFor_cep() {
+        return for_cep;
+    }
+
+    public void setFor_cep(String for_cep) {
+        this.for_cep = for_cep;
+    }
+
+    public String getFor_telefone() {
+        return for_telefone;
+    }
+
+    public void setFor_telefone(String for_telefone) {
+        this.for_telefone = for_telefone;
+    }
+
+    public String getFor_telcomercial() {
+        return for_telcomercial;
+    }
+
+    public void setFor_telcomercial(String for_telcomercial) {
+        this.for_telcomercial = for_telcomercial;
+    }
+    
+    
+    
+    
+}
